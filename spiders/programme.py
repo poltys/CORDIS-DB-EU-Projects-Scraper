99b50df6306e17de842e68afaa75255d954d79c9
Simon Hardy <shardy@linux.fritz.box>
2018-01-31 14:32:35 +0100

    WP Draft

--- spiders/programme.py
+++ spiders/programme.py
@@ -5,7 +5,17 @@ import scrapy
 class ProgrammeSpider(scrapy.Spider):
     name = 'programme'
     allowed_domains = ['cordis.europa.eu']
-    start_urls = ['http://cordis.europa.eu/projects/result_en?q=(relatedProgramme/programme/code%3D%27H2020-EU.1.1.*%27%20OR%20relatedSubProgramme/programme/code%3D%27H2020-EU.1.1.*%27)%20AND%20contenttype%3D%27project%27']
+    start_urls = ['http://cordis.europa.eu/projects/home_en.html']
+
+    # BASE_URL = 'http://cordis.europa.eu/projects/'
 
     def parse(self, response):
-        pass
+        links = response.xpath('//*[@id="cattree"]/div[2]/div[1]/dl/dd[1]/a').extract()
+        for link in links:
+            absolute_url = self.BASE_URL = link
+            yield scrapy.Request(absolute_url, callback=self.parse_attr)
+
+    def parse_attr(self, response):
+        item = ProgrammeItem()
+        item['link'] = response.url
+        yield item
